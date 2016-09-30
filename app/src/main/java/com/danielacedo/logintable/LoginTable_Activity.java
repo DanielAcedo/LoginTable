678c367fe56a51f1e3eb05dfc192b7018a16d7c4
Daniel Acedo Calderon <DanielExMachina0451@gmail.com>
2016-09-30 14:28:30 +0200

    Initial commit

---
+++ app/src/main/java/com/danielacedo/logintable/LoginTable_Activity.java
@@ -0,0 +1,13 @@
+package com.danielacedo.logintable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginTable_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_table_);
+    }
+}
