1d33725572de00949e5ea09d31adeb225a747bf7
Daniel Acedo Calderon <DanielExMachina0451@gmail.com>
2016-10-05 14:44:38 +0200

    Added a controller for the login view and implemented an error code identification

--- app/src/main/java/com/danielacedo/logintable/LoginTable_Activity.java
+++ app/src/main/java/com/danielacedo/logintable/LoginTable_Activity.java
@@ -2,12 +2,71 @@ package com.danielacedo.logintable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.danielacedo.logintable.controller.LoginTable_Controller;
+
+import static android.text.TextUtils.isEmpty;
 
 public class LoginTable_Activity extends AppCompatActivity {
+
+    private LoginTable_Controller loginTable_Controller;
+    private EditText edt_User, edt_Password;
+    private Button btn_Ok, btn_Cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_table_);
+        loginTable_Controller = new LoginTable_Controller();
+        edt_User = (EditText)findViewById(R.id.edt_User);
+        edt_Password = (EditText)findViewById(R.id.edt_Password);
+        btn_Ok = (Button)findViewById(R.id.btn_Ok);
+        btn_Ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edt_User.getText().toString();
+                String pass = edt_Password.getText().toString();
+
+                if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass)){
+                    Toast.makeText(LoginTable_Activity.this, getResources().getString(R.string.err_emptyData), Toast.LENGTH_SHORT).show();
+                }else{
+                    int result = loginTable_Controller.validateCredentials(user, pass);
+
+                    switch (result){
+                        case LoginTable_Controller.PASSWORD_DIGIT:
+                            Toast.makeText(LoginTable_Activity.this, getResources().getString(R.string.err_Password_Digit),Toast.LENGTH_SHORT).show();
+                            break;
+                        case LoginTable_Controller.PASSWORD_UPPERLOWERCASE:
+                            Toast.makeText(LoginTable_Activity.this, getResources().getString(R.string.err_Password_UpperLowerCase),Toast.LENGTH_SHORT).show();
+                            break;
+                        case LoginTable_Controller.PASSWORD_LENGTH:
+                            Toast.makeText(LoginTable_Activity.this, getResources().getString(R.string.err_Password_Length),Toast.LENGTH_SHORT).show();
+                            break;
+                        case LoginTable_Controller.OK:
+                            // The activity would be started after login
+
+                            break;
+                    }
+                }
+            }
+        });
+
+        btn_Cancel = (Button)findViewById(R.id.btn_Cancel);
+        btn_Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetValues();
+            }
+        });
+    }
+
+    private void resetValues() {
+        edt_User.setText("");
+        edt_Password.setText("");
     }
 }
---
+++ app/src/main/java/com/danielacedo/logintable/controller/LoginTable_Controller.java
@@ -0,0 +1,38 @@
+package com.danielacedo.logintable.controller;
+
+/**
+ * Created by usuario on 5/10/16.
+ */
+
+import android.media.MediaCodec;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.regex.Pattern;
+
+/**
+ *
+ */
+
+public class LoginTable_Controller{
+
+    public static final int OK = 0; // Success
+    public static final int PASSWORD_DIGIT = 1; //The password doesn't comply with the password minimum digit policy
+    public static final int PASSWORD_UPPERLOWERCASE = 2; //The password doesn't comply with the password minimum lowercase/uppercase policy
+    public static final int PASSWORD_LENGTH = 3; //The password doesn't comply with the password length policy
+
+    public int validateCredentials(String user, String pass){
+        if(!Pattern.matches(".*[0-9].*", pass)){
+            return PASSWORD_DIGIT;
+        }
+
+        if(!Pattern.matches(".*[a-z].*",pass) || !Pattern.matches(".*[A-Z].*",pass)){
+            return PASSWORD_UPPERLOWERCASE;
+        }
+
+        if(pass.length()<8){
+            return PASSWORD_LENGTH;
+        }
+
+        return OK;
+    }
+}
