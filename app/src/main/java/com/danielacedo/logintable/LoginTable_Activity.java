b8bfbfdeff50d169f0c471c21418e0f64a4a3c9c
Daniel Acedo Calderon <DanielExMachina0451@gmail.com>
2016-10-06 08:54:38 +0200

    Included generalization for the controller class using an interface

--- app/src/main/java/com/danielacedo/logintable/LoginTable_Activity.java
+++ app/src/main/java/com/danielacedo/logintable/LoginTable_Activity.java
@@ -8,13 +8,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.danielacedo.logintable.controller.ILoginMvc;
 import com.danielacedo.logintable.controller.LoginTable_Controller;
-
-import static android.text.TextUtils.isEmpty;
 
 public class LoginTable_Activity extends AppCompatActivity {
 
-    private LoginTable_Controller loginTable_Controller;
+    private ILoginMvc loginMvc;
     private EditText edt_User, edt_Password;
     private Button btn_Ok, btn_Cancel;
 
@@ -22,7 +21,7 @@ public class LoginTable_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_table_);
-        loginTable_Controller = new LoginTable_Controller();
+        loginMvc = new LoginTable_Controller();
         edt_User = (EditText)findViewById(R.id.edt_User);
         edt_Password = (EditText)findViewById(R.id.edt_Password);
         btn_Ok = (Button)findViewById(R.id.btn_Ok);
@@ -35,7 +34,7 @@ public class LoginTable_Activity extends AppCompatActivity {
                 if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass)){
                     Toast.makeText(LoginTable_Activity.this, getResources().getString(R.string.err_emptyData), Toast.LENGTH_SHORT).show();
                 }else{
-                    int result = loginTable_Controller.validateCredentials(user, pass);
+                    int result = ((LoginTable_Controller)loginMvc).validateCredentials(user, pass);
 
                     switch (result){
                         case LoginTable_Controller.PASSWORD_DIGIT:
