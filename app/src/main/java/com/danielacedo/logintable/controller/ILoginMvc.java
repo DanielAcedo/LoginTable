2aac24ae12f679fd9ae1c88e96141b112cd76ca4
Daniel Acedo Calderon <DanielExMachina0451@gmail.com>
2016-10-06 08:55:51 +0200

    Included generalization for the controller class using an interface

---
+++ app/src/main/java/com/danielacedo/logintable/controller/ILoginMvc.java
@@ -0,0 +1,9 @@
+package com.danielacedo.logintable.controller;
+
+/**
+ * Created by usuario on 6/10/16.
+ */
+
+public interface ILoginMvc {
+    public int validateCredentials(String user, String pass);
+}
